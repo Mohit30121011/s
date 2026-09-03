78175f6f0013d748489b6b63deffd14a1aef079e
Mohit <mohit@nlogistic.com>
2026-09-03 22:28:44 +0530

    Fix timezone skew in live tracking: Set JDBC serverTimezone to Asia/Kolkata so timestamps match real local time instead of jumping 5:30h forward

--- src/main/java/com/nlogistic/util/DBConnectionManager.java
+++ src/main/java/com/nlogistic/util/DBConnectionManager.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnectionManager {
-	private static final String URL = "jdbc:mysql://localhost:3306/nlogistic_db?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+	private static final String URL = "jdbc:mysql://localhost:3306/nlogistic_db?useSSL=false&serverTimezone=Asia/Kolkata&allowPublicKeyRetrieval=true";
 	private static final String USER = "root";
 	private static final String PASSWORD = "NewPassword123!";                
 
